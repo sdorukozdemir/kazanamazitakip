f567cbf19850b0db6264b3ebc8511921ee8c984e
sdorukozdemir <147822849+sdorukozdemir@users.noreply.github.com>
2026-03-11 10:38:09 +0300

    Update CACHE_NAME to version 1.01

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Sadece bu satırı değiştirin (Örn: v3)
-const CACHE_NAME = 'ibadet-takip-v4-final'; 
+const CACHE_NAME = 'ibadet-takip-v1.01'; 
 
 // Geri kalan kodlar aynı kalabilir...
 const STATIC_ASSETS = [
@@ -54,6 +54,7 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
+
 
 
 
